7ac7d4f93143cea6bf4d9ecbb69658a71be784ea
Gilad Zelzer <giladz@microsoft.com>
2023-05-03 20:49:50 +0300

    wip

---
+++ logic.py
@@ -0,0 +1,6 @@
+def logic_print(val : str):
+    print("Yo " + val)
+
+
+def logic_two_print(val : str):
+    print("Main " + val)
---
+++ main.py
@@ -0,0 +1,12 @@
+
+from logic import logic_print
+
+def test_print(val : str):
+    print("Hey " + val)
+    
+value = "gilad"
+test_print(value)
+
+logic_print(value)
+
+
